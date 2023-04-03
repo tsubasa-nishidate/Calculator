a5285d89e39c8401d2871dab023c55d395b8c2f7
tsubasa-nishidate <datesan1997729@gmail.com>
2023-04-03 11:19:11 +0900

    first commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by 西舘翼 on 2023/04/03.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
